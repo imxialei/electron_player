e1ce01b73e220f15af37ad38fe60c97472b61922
夏磊 <xiallei@outlook.com>
2019-06-10 10:27:39 +0800

    initial

---
+++ main.js
@@ -0,0 +1,56 @@
+const {app, BrowserWindow, Notification, Menu} = require('electron')
+const fs = require('fs')
+const log = console.log.bind(console)
+const createWindow = function() {
+
+    mainWindow = new BrowserWindow({
+        width: 330,
+        height: 480,
+        // transparent: true,
+        // frame: false,
+        webPreferences: {
+            nodeIntegrationInWorker: true,
+        },
+    })
+
+    // mainWindow.setMenu(null)
+    mainWindow.loadFile('index.html')
+    mainWindow.on('close', function() {
+        mainWindow = null
+    })
+
+    /*隐藏electron创听的菜单栏*/
+    // Menu.setApplicationMenu(null)
+
+    let notification = new Notification()
+    notification.show()
+}
+
+const loadMainWindow = function() {
+    let mainWindow
+    // let Menu = electron.Menu
+
+    app.on('ready', createWindow)
+
+    app.on('window-all-closed', function() {
+        app.quit()
+    })
+}
+
+const loadRootFiles = function() {
+    fs.readdir('.', (err, files) => {
+        log('typeof', typeof err, err)
+        if (err !== null) {
+            log(err)
+        } else {
+            log('files', files)
+        }})
+    log('load 运行')
+}
+
+const __main = function() {
+    loadMainWindow()
+    loadRootFiles()
+}
+
+__main()
